de2a051fe8d0d8e6cb42a0b06208dd6589657aca
riyaya1528 <46711875+riyaya1528@users.noreply.github.com>
2021-08-26 15:17:44 +0900

    Add files via upload

--- src/main/java/riyaya1528/paper/foreverbedrock/Commands.java
+++ src/main/java/riyaya1528/paper/foreverbedrock/Commands.java
@@ -9,24 +9,28 @@ public class Commands implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(command.getName().equals("bedrock")) {
-            if (args[0].length() != 0) {
-                if(args[0].equals("true")) {
-                    sender.sendMessage("§a§l有効にしました");
-                    ForeverBedRock.instance.getConfig().set("allow","true");
-                    return true;
-                }
-                if(args[0].equals("false")) {
-                    sender.sendMessage("§a§l無効にしました");
-                    ForeverBedRock.instance.getConfig().set("allow","false");
-                    return true;
-                }
-                if(Bukkit.getServer().getWorld(args[0]) != null) {
-                    ForeverBedRock.instance.getConfig().set("world",args[0]);
-                }else {
+            if (sender.isOp()) {
+                if (args[0].length() != 0) {
+                    if (args[0].equals("true")) {
+                        sender.sendMessage("§a§l有効にしました");
+                        ForeverBedRock.instance.getConfig().set("allow", "true");
+                        ForeverBedRock.instance.saveConfig();
+                        return true;
+                    }
+                    if (args[0].equals("false")) {
+                        sender.sendMessage("§a§l無効にしました");
+                        ForeverBedRock.instance.getConfig().set("allow", "false");
+                        ForeverBedRock.instance.saveConfig();
+                        return true;
+                    }
+                    if (Bukkit.getServer().getWorld(args[0]) != null) {
+                        ForeverBedRock.instance.getConfig().set("world", args[0]);
+                    } else {
+                        sender.sendMessage("存在するワールド名を入れてください");
+                    }
+                } else {
                     sender.sendMessage("存在するワールド名を入れてください");
                 }
-            }else {
-                sender.sendMessage("存在するワールド名を入れてください");
             }
         }
         return false;
